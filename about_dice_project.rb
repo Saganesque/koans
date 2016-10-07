02391a74876f18ac85e679b92c84b6abb349256e
Saganesque <forvendorsonly@gmail.com>
2016-10-07 00:50:39 -0700

    last commit before branching. about_inheritance will be partly done at this point

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+ class DiceSet
+
+   @values = 0
+
+   def roll(die_count)
+     die_set = []
+     die_count.times do 
+       the_roll = 1 + rand(6) 
+       @values = die_set.push(the_roll)
+     end
+   end
+
+   attr_reader :values
+
+  # def size
+  #   @values.size
+  # end
+
+
+ end #end DiceSet class
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
